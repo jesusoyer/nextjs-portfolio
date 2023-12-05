983fe52aa7f7fbcce740bb83c4275fc8a68d8c55
jesusoyer <oyervidesjesus017@gmail.com>
2023-12-05 15:37:16 -0600

    added code for contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
+import ContactPage from "@/components/ContactPage";
 export default function contacts() {
   return (
    <main>
     <Navbar />
-   <div>this is the contacts page</div>
+   <ContactPage />
    <Footer />
    </main>
   )
