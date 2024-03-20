307bb3eaf498d2ce205f2a515733475bb27fade8
jesusoyer <oyervidesjesus017@gmail.com>
2024-03-20 12:44:03 -0500

    turned of api code

--- pages/api/send.tsx
+++ pages/api/send.tsx
@@ -1,26 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/EmailTemplate';
-import { Resend } from 'resend';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { EmailTemplate } from '../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { body } = req
-  const { data, error } = await resend.emails.send(
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//     const { body } = req
+//   const { data, error } = await resend.emails.send(
     
-    {
-    from: "jesus <jesus@jesoyer.com>",
-    to: [body.email],
-    subject: body.message,
-    react: EmailTemplate({ firstName: body.name }),
-    text:'' ,
-  } 
-  );
+//     {
+//     from: "jesus <jesus@jesoyer.com>",
+//     to: [body.email],
+//     subject: body.message,
+//     react: EmailTemplate({ firstName: body.name }),
+//     text:'' ,
+//   } 
+//   );
   
-  if (error) {
-    return res.status(400).json(error);
-  }
+//   if (error) {
+//     return res.status(400).json(error);
+//   }
 
-  res.status(200).json(data);
-  console.log(body)
-};
+//   res.status(200).json(data);
+//   console.log(body)
+// };
